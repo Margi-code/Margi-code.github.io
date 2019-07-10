2b9db8c19999bde6b964194a51cb0f73abcb63a2
Gilheany <gilhem@8c8590172ff1.ant.amazon.com>
2019-07-10 14:34:08 -0700

    Initial commit

---
+++ Java.js
@@ -0,0 +1,9 @@
+function myfunction() {
+    alert("This is Java script!")
+// }
+
+function replaceImage() {
+	document.getElementById("daffadil-countryside-background").src="https://www.shutterstock.com/image-photo/daffodil-countryside-background-1026430984?irgwc=1&utm_medium=Affiliate&utm_campaign=pix"
+}
+function Image() {
+	document.getElementById("yellow-dutch-daffodil-flowers-close-low").src=https://www.shutterstock.com/image-photo/yellow-dutch-daffodil-flowers-close-low-1302103978?irgwc=1&utm_medium=Affiliate&utm_campaign=Pixabay+GmbH&utm_source=44814&utm_term=https%3A%2F%2Fpixabay.com%2Fphotos%2Fsearch%2Fdaffodil%2F
